6120f598847585f0dd51cbd319b1443704622059
Sourabh Raha <70322684+sourabhraha@users.noreply.github.com>
2022-06-10 00:46:41 +0530

    Added solution - LeetHub

---
+++ Sieve of Eratosthenes - GFG/sieve-of-eratosthenes.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    vector<int> sieveOfEratosthenes(int N)
+    {
+        int primes[N+1] = {0};
+        for(int i=2; i<=N; i++)
+        {
+            if(primes[i] == 0)
+            {
+                for(int j= i*i; j<= N; j += i)
+                    primes[j] = 1;
+            }
+        }
+        vector<int>ans;
+        
+        for(int i=2; i<=N; i++)
+        {
+            if(primes[i] == 0) ans.push_back(i);
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        vector<int> primes  = ob.sieveOfEratosthenes(N);
+        for(auto prime : primes) {
+            cout<< prime <<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
