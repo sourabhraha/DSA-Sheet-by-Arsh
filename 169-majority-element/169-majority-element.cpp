2274c4801115b366048beaf9e8114727076b0e20
Sourabh Raha <70322684+sourabhraha@users.noreply.github.com>
2022-07-18 02:43:22 +0530

    Time: 22 ms (82.68%), Space: 19.5 MB (92.00%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int>ump;
-        
-        for(auto it:nums) ump[it]++;
-        int ans = 0, curr = 0;
-        for(auto it:ump) if(it.second > curr){ ans = it.first; curr = it.second; }
-        return ans;
+            int candidate = -1, votes = 0;
+            for(auto it:nums)
+            {
+                if(votes == 0)  candidate = it;
+                votes += (candidate == it) ? 1 : -1;
+            }
+           return candidate;
     }
 };
