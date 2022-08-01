7f22a9cf8fe48da4a1272b8871aad7a7f172cb4d
Sourabh Raha <70322684+sourabhraha@users.noreply.github.com>
2022-08-01 15:56:50 +0530

    Time: 26 ms (46.05%), Space: 14.3 MB (57.53%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,46 +9,16 @@
  */
 class Solution {
 public:
-    
-    bool getPath(TreeNode *root, vector<pair<TreeNode*, int>>&path, TreeNode *node)
-    {
-        if(!root) return false;
-        path.push_back({root, root->val});
-        
-        if(root == node) return true;
-        if(getPath(root->left, path, node) || 
-           getPath(root->right, path, node))
-            return true;
-        path.pop_back();
-        return false;
-    }
-
-    
-    
-    
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+     
+        if(!root || root == p || root == q) return root;
         
-        if(!root) return root;
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
         
-        vector<pair<TreeNode*, int>>pPath, qPath;
+        if(left == NULL) return right;
+        else if(right == NULL) return left;
+        else return root;
         
-        getPath(root, pPath, p);
-        getPath(root, qPath, q);
-        
-        auto itP = pPath.begin(), itQ = qPath.begin();
-        
-        TreeNode *ans = NULL;
-        while(itP != pPath.end() || itQ != qPath.end())
-        {
-           if(itP == pPath.end() || itQ == qPath.end()) break;
-           if(itP->second == itQ->second)
-                ans = itP->first;
-            itP++;
-            itQ++;
-        }
-        
- 
-        
-        return ans;
     }
 };
