991166ab61e0cbc7c7cdfa721233fa397f75dfd6
Sourabh Raha <70322684+sourabhraha@users.noreply.github.com>
2022-08-17 00:09:59 +0530

    Time: 18 ms (17.10%), Space: 5.8 MB (95.25%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -2,6 +2,18 @@ class Solution {
 public:
     int mySqrt(int x) {
         
-        return int(sqrt(x));
+        if(x == 0 || x == 1)
+            return x;
+        
+        long long  st = 1;
+        long long  res = 1;
+ 
+        
+        while(res <= x)
+        {
+           st++;
+           res = st*st;
+        }
+         return st-1;
     }
 };
