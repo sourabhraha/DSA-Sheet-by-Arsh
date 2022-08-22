0473c39c51cd89412f6ea53afa689643cd15d0b7
Sourabh Raha <70322684+sourabhraha@users.noreply.github.com>
2022-08-22 20:38:46 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
+    
     bool isPowerOfFour(int n) {
-       
-        if(n <= 0 || (n%4 != 0 && n!= 1))
-             return false;
-        if(n == 1)
-            return true;
-        return isPowerOfFour(n/4);
+        
+        if(n <= 0) return false;
+        if(n == 1) return true;
+        
+        while(n > 0)
+        {
+            if(n%4 != 0 && n != 1)
+                return false;
+            n /= 4;
+        }
+        return true;
     }
 };
